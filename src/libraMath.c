3db95167386ffb771fc5eadb9a69043ecd398fbb
Fraz Mahmud <fraz501@hotmail.com>
2025-06-21 15:03:31 -0400

    added matrix multiplication and an alternative initialization

--- src/libraMath.c
+++ src/libraMath.c
@@ -111,20 +111,141 @@ void LC_Vector4DNormalize(LC_Vector4D vec4) {
 }
 
 void LC_Matrix2DInitialize(LC_Matrix2D mat2, const LC_Vector2D a, const LC_Vector2D b) {
-    mat2[0][0] = a[0]; mat2[0][1] = a[1];
-    mat2[1][0] = b[0]; mat2[1][1] = b[1];
+    mat2[0][0] = a[0];
+    mat2[0][1] = a[1];
+    mat2[1][0] = b[0];
+    mat2[1][1] = b[1];
+}
+
+void LC_Matrix2DInitializeF(const float n00, const float n01,
+                            const float n10, const float n11, LC_Matrix2D mat2) {
+    mat2[0][0] = n00;
+    mat2[0][1] = n10;
+    mat2[1][0] = n01;
+    mat2[1][1] = n11;
 }
 
 void LC_Matrix3DInitialize(LC_Matrix3D mat3, const LC_Vector3D a, const LC_Vector3D b, const LC_Vector3D c) {
-    mat3[0][0] = a[0]; mat3[0][1] = a[1]; mat3[0][2] = a[2];
-    mat3[1][0] = b[0]; mat3[1][1] = b[1]; mat3[1][2] = b[2];
-    mat3[2][0] = c[0]; mat3[2][1] = c[1]; mat3[2][2] = c[2];
+    mat3[0][0] = a[0];
+    mat3[0][1] = a[1];
+    mat3[0][2] = a[2];
+    mat3[1][0] = b[0];
+    mat3[1][1] = b[1];
+    mat3[1][2] = b[2];
+    mat3[2][0] = c[0];
+    mat3[2][1] = c[1];
+    mat3[2][2] = c[2];
+}
+
+void LC_Matrix3DInitializeF(const float n00, const float n01, const float n02,
+                            const float n10, const float n11, const float n12,
+                            const float n20, const float n21, const float n22, LC_Matrix3D mat3) {
+    mat3[0][0] = n00;
+    mat3[0][1] = n10;
+    mat3[0][2] = n20;
+    mat3[1][0] = n01;
+    mat3[1][1] = n11;
+    mat3[1][2] = n21;
+    mat3[2][0] = n02;
+    mat3[2][1] = n12;
+    mat3[2][2] = n22;
 }
 
 void LC_Matrix4DInitialize(LC_Matrix4D mat4, const LC_Vector4D a, const LC_Vector4D b, const LC_Vector4D c,
-    const LC_Vector4D d) {
-    mat4[0][0] = a[0]; mat4[0][1] = a[1]; mat4[0][2] = a[2]; mat4[0][3] = a[3];
-    mat4[1][0] = b[0]; mat4[1][1] = b[1]; mat4[1][2] = b[2]; mat4[1][3] = b[3];
-    mat4[2][0] = c[0]; mat4[2][1] = c[1]; mat4[2][2] = c[2]; mat4[2][3] = c[3];
-    mat4[3][0] = d[0]; mat4[3][1] = d[1]; mat4[3][2] = d[2]; mat4[3][3] = d[3];
+                           const LC_Vector4D d) {
+    mat4[0][0] = a[0];
+    mat4[0][1] = a[1];
+    mat4[0][2] = a[2];
+    mat4[0][3] = a[3];
+    mat4[1][0] = b[0];
+    mat4[1][1] = b[1];
+    mat4[1][2] = b[2];
+    mat4[1][3] = b[3];
+    mat4[2][0] = c[0];
+    mat4[2][1] = c[1];
+    mat4[2][2] = c[2];
+    mat4[2][3] = c[3];
+    mat4[3][0] = d[0];
+    mat4[3][1] = d[1];
+    mat4[3][2] = d[2];
+    mat4[3][3] = d[3];
+}
+
+void LC_Matrix4DInitializeF(const float n00, const float n01, const float n02, const float n03,
+                            const float n10, const float n11, const float n12, const float n13,
+                            const float n20, const float n21, const float n22, const float n23,
+                            const float n30, const float n31, const float n32, const float n33, LC_Matrix4D mat4) {
+    mat4[0][0] = n00;
+    mat4[0][1] = n10;
+    mat4[0][2] = n20;
+    mat4[0][3] = n30;
+    mat4[1][0] = n01;
+    mat4[1][1] = n11;
+    mat4[1][2] = n21;
+    mat4[1][3] = n31;
+    mat4[2][0] = n02;
+    mat4[2][1] = n12;
+    mat4[2][2] = n22;
+    mat4[2][3] = n32;
+    mat4[3][0] = n03;
+    mat4[3][1] = n13;
+    mat4[3][2] = n23;
+    mat4[3][3] = n33;
+}
+
+void LC_Matrix2DMulVector2D(LC_Matrix2D mat2, const LC_Vector2D vec2, LC_Vector2D destination) {
+    destination[0] = mat2[0][0] * vec2[0] + mat2[1][0] * vec2[1];
+    destination[1] = mat2[0][1] * vec2[0] + mat2[1][1] * vec2[1];
+}
+
+void LC_Matrix3DMulVector3D(LC_Matrix3D mat3, const LC_Vector3D vec3, LC_Vector3D destination) {
+    destination[0] = mat3[0][0] * vec3[0] + mat3[1][0] * vec3[1] + mat3[2][0] * vec3[2];
+    destination[1] = mat3[0][1] * vec3[0] + mat3[1][1] * vec3[1] + mat3[2][1] * vec3[2];
+    destination[2] = mat3[0][2] * vec3[0] + mat3[1][2] * vec3[1] + mat3[2][2] * vec3[2];
+}
+
+void LC_Matrix4DMulVector4D(LC_Matrix4D mat4, const LC_Vector4D vec4, LC_Vector4D destination) {
+    destination[0] = mat4[0][0] * vec4[0] + mat4[1][0] * vec4[1] + mat4[2][0] * vec4[2] + mat4[3][0] * vec4[3];
+    destination[1] = mat4[0][1] * vec4[0] + mat4[1][1] * vec4[1] + mat4[2][1] * vec4[2] + mat4[3][1] * vec4[3];
+    destination[2] = mat4[0][2] * vec4[0] + mat4[1][2] * vec4[1] + mat4[2][2] * vec4[2] + mat4[3][2] * vec4[3];
+    destination[3] = mat4[0][3] * vec4[0] + mat4[1][3] * vec4[1] + mat4[2][3] * vec4[2] + mat4[3][3] * vec4[3];
+}
+
+void LC_Matrix2DMulMatrix2D(LC_Matrix2D a, LC_Matrix2D b, LC_Matrix2D destination) {
+    LC_Matrix2DInitializeF(a[0][0] * b[0][0] + a[1][0] * b[0][1],
+                           a[0][0] * b[1][0] + a[1][0] * b[1][1],
+                           a[0][1] * b[0][0] + a[1][1] * b[0][1],
+                           a[0][1] * b[1][0] + a[1][1] * b[1][1], destination);
+}
+
+void LC_Matrix3DMulMatrix3D(LC_Matrix3D a, LC_Matrix3D b, LC_Matrix3D destination) {
+    LC_Matrix3DInitializeF(a[0][0] * b[0][0] + a[1][0] * b[0][1] + a[2][0] * b[0][2],
+                           a[0][0] * b[1][0] + a[1][0] * b[1][1] + a[2][0] * b[1][2],
+                           a[0][0] * b[2][0] + a[1][0] * b[2][1] + a[2][0] * b[2][2],
+                           a[0][1] * b[0][0] + a[1][1] * b[0][1] + a[2][1] * b[0][2],
+                           a[0][1] * b[1][0] + a[1][1] * b[1][1] + a[2][1] * b[1][2],
+                           a[0][1] * b[2][0] + a[1][1] * b[2][1] + a[2][1] * b[2][2],
+                           a[0][2] * b[0][0] + a[1][2] * b[0][1] + a[2][2] * b[0][2],
+                           a[0][2] * b[1][0] + a[1][2] * b[1][1] + a[2][2] * b[1][2],
+                           a[0][2] * b[2][0] + a[1][2] * b[2][1] + a[2][2] * b[2][2], destination);
+}
+
+void LC_Matrix4DMulMatrix4D(LC_Matrix4D a, LC_Matrix4D b, LC_Matrix4D destination) {
+    LC_Matrix4DInitializeF(a[0][0] * b[0][0] + a[1][0] * b[0][1] + a[2][0] * b[0][2] + a[3][0] * b[0][3],
+                           a[0][0] * b[1][0] + a[1][0] * b[1][1] + a[2][0] * b[1][2] + a[3][0] * b[1][3],
+                           a[0][0] * b[2][0] + a[1][0] * b[2][1] + a[2][0] * b[2][2] + a[3][0] * b[2][3],
+                           a[0][0] * b[3][0] + a[1][0] * b[3][1] + a[2][0] * b[3][2] + a[3][0] * b[3][3],
+                           a[0][1] * b[0][0] + a[1][1] * b[0][1] + a[2][1] * b[0][2] + a[3][1] * b[0][3],
+                           a[0][1] * b[1][0] + a[1][1] * b[1][1] + a[2][1] * b[1][2] + a[3][1] * b[1][3],
+                           a[0][1] * b[2][0] + a[1][1] * b[2][1] + a[2][1] * b[2][2] + a[3][1] * b[2][3],
+                           a[0][1] * b[3][0] + a[1][1] * b[3][1] + a[2][1] * b[3][2] + a[3][1] * b[3][3],
+                           a[0][2] * b[0][0] + a[1][2] * b[0][1] + a[2][2] * b[0][2] + a[3][2] * b[0][3],
+                           a[0][2] * b[1][0] + a[1][2] * b[1][1] + a[2][2] * b[1][2] + a[3][2] * b[1][3],
+                           a[0][2] * b[2][0] + a[1][2] * b[2][1] + a[2][2] * b[2][2] + a[3][2] * b[2][3],
+                           a[0][2] * b[3][0] + a[1][2] * b[3][1] + a[2][2] * b[3][2] + a[3][2] * b[3][3],
+                           a[0][3] * b[0][0] + a[1][3] * b[0][1] + a[2][3] * b[0][2] + a[3][3] * b[0][3],
+                           a[0][3] * b[1][0] + a[1][3] * b[1][1] + a[2][3] * b[1][2] + a[3][3] * b[1][3],
+                           a[0][3] * b[2][0] + a[1][3] * b[2][1] + a[2][3] * b[2][2] + a[3][3] * b[2][3],
+                           a[0][3] * b[3][0] + a[1][3] * b[3][1] + a[2][3] * b[3][2] + a[3][3] * b[3][3],
+                           destination);
 }
--- src/libraMath.h
+++ src/libraMath.h
@@ -33,8 +33,23 @@ void LC_Vector3DNormalize(LC_Vector3D vec3);
 void LC_Vector4DNormalize(LC_Vector4D vec4);
 
 void LC_Matrix2DInitialize(LC_Matrix2D mat2, const LC_Vector2D a, const LC_Vector2D b);
+void LC_Matrix2DInitializeF(float n00, float n01,
+                            float n10, float n11, LC_Matrix2D mat2);
 void LC_Matrix3DInitialize(LC_Matrix3D mat3, const LC_Vector3D a, const LC_Vector3D b, const LC_Vector3D c);
+void LC_Matrix3DInitializeF(float n00, float n01, float n02,
+                            float n10, float n11, float n12,
+                            float n20, float n21, float n22, LC_Matrix3D mat3);
 void LC_Matrix4DInitialize(LC_Matrix4D mat4, const LC_Vector4D a, const LC_Vector4D b, const LC_Vector4D c,
     const LC_Vector4D d);
+void LC_Matrix4DInitializeF(float n00, float n01, float n02, float n03,
+                            float n10, float n11, float n12, float n13,
+                            float n20, float n21, float n22, float n23,
+                            float n30, float n31, float n32, float n33, LC_Matrix4D mat4);
+void LC_Matrix2DMulVector2D(LC_Matrix2D mat2, const LC_Vector2D vec2, LC_Vector2D destination);
+void LC_Matrix3DMulVector3D(LC_Matrix3D mat3, const LC_Vector3D vec3, LC_Vector3D destination);
+void LC_Matrix4DMulVector4D(LC_Matrix4D mat4, const LC_Vector4D vec4, LC_Vector4D destination);
+void LC_Matrix2DMulMatrix2D(LC_Matrix2D a, LC_Matrix2D b, LC_Matrix2D destination);
+void LC_Matrix3DMulMatrix3D(LC_Matrix3D a, LC_Matrix3D b, LC_Matrix3D destination);
+void LC_Matrix4DMulMatrix4D(LC_Matrix4D a, LC_Matrix4D b, LC_Matrix4D destination);
 
 #endif //LIBRAMATH_H
